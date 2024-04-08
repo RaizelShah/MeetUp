5795a77121bd04bcb30a6074a54b20be941b61cc
RaizelShah <shahraizel11@gmail.com>
2024-04-08 23:24:48 +0530

    Update Sidebar

---
+++ constants/index.ts
@@ -0,0 +1,27 @@
+export const sidebarLinks = [
+  {
+    Label: "Home",
+    route: "/",
+    imgUrl: "/icons/Home.svg",
+  },
+  {
+    Label: "Upcoming",
+    route: "/upcoming",
+    imgUrl: "/icons/upcoming.svg",
+  },
+  {
+    Label: "Previous",
+    route: "/previous",
+    imgUrl: "/icons/previous.svg",
+  },
+  {
+    Label: "Recordings",
+    route: "/recordings",
+    imgUrl: "/icons/Video.svg",
+  },
+  {
+    Label: "Personal Room",
+    route: "/personal-room",
+    imgUrl: "/icons/add-personal.svg",
+  }
+];

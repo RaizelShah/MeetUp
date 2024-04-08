a2f1a6e9ac7a06735d4a018c19cb0b2fb7a50e9f
RaizelShah <shahraizel11@gmail.com>
2024-04-08 18:39:29 +0530

    project setup

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,11 @@
+import React, { ReactNode } from 'react'
+
+const layout = ({children}:{children:ReactNode}) => {
+  return (
+    <main>
+        {children}
+    </main>
+  )
+}
+
+export default layout
---
+++ components/Sidebar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <section className='sticky left-0 top-0 flex h-screen w-fit flex-col justify-between bg-dark-1'>Sidebar</section>
+  )
+}
+
+export default Sidebar

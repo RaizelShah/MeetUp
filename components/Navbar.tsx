279e3114f1dc123a0be02c3924bbafc920af940b
RaizelShah <shahraizel11@gmail.com>
2024-04-09 16:23:18 +0530

    Update Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import MobileNav from "./MobileNav";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="flex-between w-full fixed z-50 bg-dark-1 px-6 py-4 lg:px-10">
+      <Link href="/" className="flex items-center gap-1">
+        <Image
+          src="/icons/logo.svg"
+          width={32}
+          height={32}
+          alt="MetUp logo"
+          className="max-sm:size-10"
+        />
+        <p className="text-[26px] font-extrabold text-white max-sm:hidden">
+          MeetUp
+        </p>
+      </Link>
 
-export default Navbar
+      <div className="flex-between gap-5">
+        {/* Clerk-User Management */}
+
+        <MobileNav />
+
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;

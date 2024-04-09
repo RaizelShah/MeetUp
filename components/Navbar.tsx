96fb0659070169aca4dca97083967c46f443fb2f
RaizelShah <shahraizel11@gmail.com>
2024-04-09 17:28:35 +0530

    add Auth

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import MobileNav from "./MobileNav";
+import { SignedIn, UserButton } from "@clerk/nextjs";
 
 const Navbar = () => {
   return (
@@ -20,7 +21,9 @@ const Navbar = () => {
       </Link>
 
       <div className="flex-between gap-5">
-        {/* Clerk-User Management */}
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
 
         <MobileNav />
 

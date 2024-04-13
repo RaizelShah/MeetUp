740531450206836b6d256c866045d87fc3fa9adc
RaizelShah <shahraizel11@gmail.com>
2024-04-13 16:00:56 +0530

    complete MeetingRoom layout

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -1,6 +1,15 @@
 import Navbar from "@/components/Navbar";
 import Sidebar from "@/components/Sidebar";
+import { Metadata } from "next";
 import React, { ReactNode } from "react";
+
+export const metadata: Metadata = {
+  title: "MeetUp",
+  description: "A video calling app",
+  icons: {
+    icon: "/icons/logo.svg",
+  },
+};
 
 const Homelayout = ({ children }: { children: ReactNode }) => {
   return (
--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,14 @@
 import StreamVideoProvider from "@/providers/StreamClientProvider";
+import { Metadata } from "next";
 import React, { ReactNode } from "react";
+
+export const metadata: Metadata = {
+  title: "MeetUp",
+  description: "A video calling app",
+  icons: {
+    icon: "/icons/logo.svg",
+  },
+};
 
 const layout = ({ children }: { children: ReactNode }) => {
   return (
